6a25555a3dfb57402d7531e404ca74191929e5f9
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-02-08 12:54:10 +0530

    Used Lombok for Logging

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollApplication.java
@@ -1,14 +1,19 @@
 package com.bridgelabz.employeepayroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Payroll Application");
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
