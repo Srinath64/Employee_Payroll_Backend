600eb7472a61d66da23ce38f9bdca3d0ee9037ac
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-02-09 08:38:21 +0530

    Introduced Custom Exception and Handler

--- src/main/java/com/bridgelabz/employeepayroll/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/service/EmployeePayrollService.java
@@ -30,7 +30,7 @@ public class EmployeePayrollService implements IEmployeePayrollService {
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData employeePayrollData = null;
-        employeePayrollData = new EmployeePayrollData(1, empPayrollDTO);
+        employeePayrollData = new EmployeePayrollData(employeePayrollList.size()+1, empPayrollDTO);
         employeePayrollList.add(employeePayrollData);
         return employeePayrollData;
     }
