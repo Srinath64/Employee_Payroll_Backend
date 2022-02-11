185812f2d754f1541770f80a5df4c84d852193f8
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-02-11 10:41:51 +0530

    Retreive all the records of Employee Payroll Pretaining to Sales Depaartment

--- src/main/java/com/bridgelabz/employeepayroll/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayroll/repository/EmployeePayrollRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
 
-    @Query(value = "select * from employee_payroll, employee_department where employee_id and department = :department", nativeQuery = true)
+    @Query(value = "select * from employee_payroll, employee_department where employee_id = id and department = :department", nativeQuery = true)
     List<EmployeePayrollData> findEmployeeByDepartment(String department);
 
 }
